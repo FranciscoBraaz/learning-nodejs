4e205b1cba519f99499314f6b6b5c907f8df6842
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-11-23 20:40:55 -0300

    feat: create middleware to check auth

--- authentication/src/routes/index.ts
+++ authentication/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import * as Api from "../controllers/apiController"
+import { Auth } from "../middlewares/auth"
 
 const router = Router()
 
@@ -7,6 +8,6 @@ router.get("/", (req, res) => {
   res.json({ message: "Bem vindo" })
 })
 
-router.get("/list", Api.list)
+router.get("/list", Auth.private, Api.list)
 
 export default router
