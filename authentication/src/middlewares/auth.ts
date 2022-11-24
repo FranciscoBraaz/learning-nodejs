728f4cd7ad51f7793d0c4045f0f039bcfbbce0cb
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-11-23 21:48:22 -0300

    feat: create basic auth

--- authentication/src/middlewares/auth.ts
+++ authentication/src/middlewares/auth.ts
@@ -1,8 +1,22 @@
 import { NextFunction, Request, Response } from "express"
+import User from "../models/User"
 
 export const Auth = {
-  private: (req: Request, res: Response, next: NextFunction) => {
-    const success = true
+  private: async (req: Request, res: Response, next: NextFunction) => {
+    let success = false
+
+    if (req.headers.authorization) {
+      const hash = req.headers.authorization.substring(6)
+      const decoded = Buffer.from(hash, "base64").toString()
+      const data = decoded.split(":")
+      if (data.length === 2) {
+        const [email, password] = data
+        const user = await User.findOne({ email })
+        if (user?.password === password) {
+          success = true
+        }
+      }
+    }
 
     if (success) {
       next()
