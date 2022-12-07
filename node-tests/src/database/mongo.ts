79f076bfbcc15bafcbcbe5425ccb2308a6a270f5
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-12-07 15:43:33 -0300

    feat: setup to test model

--- node-tests/src/database/mongo.ts
+++ node-tests/src/database/mongo.ts
@@ -1,11 +1,12 @@
 import { connect } from "mongoose"
 import dotenv from "dotenv"
+import db from "./database"
 
 dotenv.config()
 
 export async function mongoConnect() {
   try {
-    await connect(process.env.MONGO_URL as string)
+    await connect(db.url)
     console.log("MongoDB conectado com sucesso!")
   } catch (error) {
     console.log("Erro de conexão com MongoDB: ", error)
