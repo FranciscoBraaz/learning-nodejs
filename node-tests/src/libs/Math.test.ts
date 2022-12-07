55b19788cd9eca2a34d5524c498cae2f3d46be00
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-12-06 21:03:59 -0300

    feat: add more test

--- node-tests/src/libs/Math.test.ts
+++ node-tests/src/libs/Math.test.ts
@@ -5,5 +5,19 @@ it("should sum two numbers correctly", () => {
 })
 
 it("should subtract two numbers correctly", () => {
-  expect(MathLib.sub(5, 2)).toBe(3)
+  expect(MathLib.sub(2, 1)).toBe(1)
+  expect(MathLib.sub(1, 2)).toBe(-1)
+  expect(MathLib.sub(0, 2)).toBe(-2)
+  expect(MathLib.sub(2, 0)).toBe(2)
+})
+
+it("should divide two numbers correctly", () => {
+  expect(MathLib.div(2, 0)).toBe(false)
+  expect(MathLib.div(4, 2)).toBe(2)
+})
+
+it("should multiply two numbers correctly", () => {
+  expect(MathLib.mult(2, 3)).toBe(6)
+  expect(MathLib.mult(4, 0)).toBe(0)
+  expect(MathLib.mult(4, -1)).toBe(-4)
 })
--- node-tests/src/libs/Math.ts
+++ node-tests/src/libs/Math.ts
@@ -6,6 +6,12 @@ export function sub(n1: number, n2: number) {
   return n1 - n2
 }
 
-export function div(n1: number, n2: number) {}
+export function div(n1: number, n2: number) {
+  if (n2 === 0) return false
 
-export function mult(n1: number, n2: number) {}
+  return n1 / n2
+}
+
+export function mult(n1: number, n2: number) {
+  return n1 * n2
+}
